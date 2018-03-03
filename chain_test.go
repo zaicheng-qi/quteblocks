9abf6ede1fd4ca821e8bf93c535bf276512a84d1
Zaicheng Qi <vmlinz@gmail.com>
2018-03-03 23:50:35 +0800

    Add tests for chain.go

--- chain_test.go
+++ chain_test.go
@@ -39,7 +39,16 @@ func TestNewBlockChain(t *testing.T) {
 		args args
 		want *BlockChain
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
+		{
+			name: "Should create a new blockchain",
+			args: args{
+				timestamp: "2009-11-10 23:00:00 +0000 UTC m=+0.000000001",
+			},
+			want: &BlockChain{
+				Blocks: []Block{*createGenesisBlock("2009-11-10 23:00:00 +0000 UTC m=+0.000000001")},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -58,13 +67,26 @@ func TestBlockChain_AppendNewBlock(t *testing.T) {
 		data      []byte
 		timestamp string
 	}
+	genesisBlock := *createGenesisBlock("2009-11-10 23:00:00 +0000 UTC m=+0.000000001")
+	timestamp := "2009-11-10 23:10:00 +0000 UTC m=+0.000000001"
 	tests := []struct {
 		name   string
 		fields fields
 		args   args
 		want   *BlockChain
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
+		{
+			name:   "Should append a new block",
+			fields: fields{Blocks: []Block{genesisBlock}},
+			args: args{
+				data:      nil,
+				timestamp: timestamp,
+			},
+			want: &BlockChain{
+				Blocks: []Block{genesisBlock, *NewBlock(1, nil, timestamp, genesisBlock.hash)},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
